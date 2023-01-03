bf402e699dcfdea2c81947fdeecde274cf70d1f7
LeLwy <louis.chancioux@gmail.com>
2023-01-03 14:00:55 +0000

    Exos sur les boucles

---
+++ exo-boucles/exo-1.js
@@ -0,0 +1,25 @@
+let i = 0;
+let y = 25;
+let z = 0;
+
+while(i <= 10){
+    
+    console.log(i);
+    i++;
+}
+
+while(y >= 20){
+    
+    console.log(y);
+    y--;
+}
+
+while(z <= 20){
+    
+    if(z%2 === 0){
+        
+        console.log(z);
+    }
+    
+    z++;
+}
---
+++ exo-boucles/exo-2.js
@@ -0,0 +1,14 @@
+let i = 0;
+
+while(i <= 10){
+    
+    if(i%2 === 0){
+        
+        console.log(`${i} pair`);
+    }else{
+        
+        console.log(`${i} impair`);
+    }
+    
+    i++
+}
