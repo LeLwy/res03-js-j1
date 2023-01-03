5cb08edac863e337482d9687aead597943c1b9ae
LeLwy <louis.chancioux@gmail.com>
2023-01-03 09:56:32 +0000

    Exos JS premier jour

---
+++ JS1.1-String/exercice1.js
@@ -0,0 +1,8 @@
+/**
+ * Déclarer une variable ayant pour type String.
+ * Afficher la valeur de cette variable dans la console du navigateur.
+ */
+
+let variable = "variable";
+
+console.log(variable);
