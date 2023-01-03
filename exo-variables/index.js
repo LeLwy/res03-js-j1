fe0a1745ddad6bde7cd9d1826e8ffa1be7f6744f
LeLwy <louis.chancioux@gmail.com>
2023-01-03 10:30:34 +0000

    Fin des exos

---
+++ exo-variables/index.js
@@ -0,0 +1,31 @@
+const character = "Legolas";
+
+console.log(character);
+
+let name = "Legolas";
+
+console.log(name);
+
+name = "Gimli";
+
+console.log(name);
+
+let bigNumber = 15652168746753429865416865167542368459874176875986;
+
+console.log(bigNumber);
+
+let decimalNumber = 5.986541053743510875653426835468574864153;
+
+console.log(decimalNumber);
+
+let negativeNumber = -653210635;
+
+console.log(negativeNumber);
+
+let yes = true;
+
+console.log(yes);
+
+let no = false;
+
+console.log(no);
